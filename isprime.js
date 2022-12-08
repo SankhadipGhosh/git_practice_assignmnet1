c8d61575b61676c742e8d6bfbf4d260fedafe05d
SankhadipGhosh <sayanghosh281@gmail.com>
2022-12-09 00:36:25 +0530

    new prime wrongly

---
+++ isprime.js
@@ -0,0 +1,14 @@
+//if a number is prime or not
+
+
+let num=17;
+let c=0;
+   for(i=1;i<num;i++){
+    if (num%i===0)
+       c=c+1;
+       }
+    if(c=2){
+      console.log("Yes it is a prime")
+    }else{
+      console.log("No it is not a prime")
+    }
